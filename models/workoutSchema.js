579419710f3150be1ee4cbb150bb55248984e68a
Christopher Marti <chrisjm093@gmail.com>
2020-07-27 21:05:55 -0700

    mongoose workout schema created

---
+++ models/workoutSchema.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+  day:{
+    type: Date,
+    default: Date.now
+  },
+
+  exercises: [{
+    type: String,
+    
+    name: {
+      type: String,
+      trim: true,
+      required: "Please enter a workout name."
+    },
+    
+    duration: Number,
+    
+    weight: Number,
+    
+    reps: Number,
+    
+    sets: Number
+  }]
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
